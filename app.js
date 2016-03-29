202182598c3533b7737ca8084b3a2fa97f03e887
ronancode <ronanlxa@gmail.com>
2016-03-28 22:55:36 -0400

    still no google or deleting images

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ function callback(response) {
 
   var end_func = function () {
     try {
-      var files = fs.readdirSync('C:/Users/Ronan Rice/Documents/Wallpapers/')
-      files.forEach(function(f) {
-        fs.unlink('C:/Users/Ronan Rice/Documents/Wallpapers/' + f)
-      });
+      var files = fs.readdirSync('D:/GitHub/random_wallpaper/');
+      // files.forEach(function(f) {
+      //   fs.unlink('D:/GitHub/random_wallpaper/' + f)
+      // });
       var raw_urls = str.match(/imgurl=.+?&/g);
       var clean_urls = raw_urls.map(function(url) {
         return url.substring(7, url.length - 1);
@@ -30,15 +30,15 @@ function callback(response) {
       var urls = clean_urls.map(decodeURIComponent);
       //var randPick = Math.floor((urls.length - 1) * Math.random());
       var url = urls[0];
-      var cmd = "C:/Program Files (x86)/GnuWin32/bin/wget.exe";
-      var img_file = "C:/Users/Ronan Rice/Documents/Wallpapers/" + random1 + "-" + random2 + ".jpg";
+      var cmd = "D:/Program Files/GnuWin32/bin/wget.exe";
+      var img_file = "D:/GitHub/random_wallpaper/" + random1 + "-" + random2 + ".jpg";
 //      var img_file = "C:/Users/Ronan Rice/Documents/Wallpapers/" + random1 + ".jpg";
       var args = ['-O', img_file, url];
       child_process.execFileSync(cmd, args, {encoding: 'utf8' });
       wget_attempts = 0;
       http_attempts = 0;     
     } catch (err) {
-      console.log(err, "sdfsdf");
+      console.log(err, "Error recieved");
       wget_attempts += 1;
       if (wget_attempts < 3) {
         foo();
@@ -51,7 +51,7 @@ function callback(response) {
 
 function randomWord() {
   var index = Math.floor(Math.random() * 19000);
-  var cmd = "C:/Program Files (x86)/GnuWin32/bin/sed.exe";
+  var cmd = "D:/Program Files/GnuWin32/bin/sed.exe";
   var args = [index + 'q;d', './words.txt'];
   var dirty_word = child_process.execFileSync(cmd, args, { encoding: 'utf8' });
   var word = dirty_word.trim();
@@ -60,16 +60,17 @@ function randomWord() {
 }
 
 function convertImg() {
-  var cmd = "C:/Program Files/ImageMagick-6.9.3-Q16/convert.exe";
-  var args = ['*.jpg', '-pointsize 25', '-gravity south', "-stroke '#000C'", '-strokewidth 4', "-annotate 0 '" + random1 + " " + random2 + "'", '-stroke none', '-fill white', "-annotate 0 '" + random1 + " " + random2 + "'", random1 + "-" + random2 + ".jpg"]
-  child_process.execFileSync(cmd, args, {encoding: 'utf8' } );
+  var args = 'convert ' + random1 + '-' + random2 + '.jpg -pointsize 25 -gravity south -stroke "#000C" -strokewidth 4 -annotate 0 "' + random1 + ' ' + random2 + '" -stroke none -fill white -annotate 0 "' + random1 + ' ' + random2 + '" ' + random1 + "-" + random2 + ".jpg";
+  child_process.execSync(args);
 }
 
 function foo() {
-  var host = 'images.search.yahoo.com';
+  //var host = 'images.search.yahoo.com';
+  var host = 'images.google.com';
   random1 = randomWord();
   random2 = randomWord();
-  var path = '/search/images?p=' + random1 + '+' + random2 + '&imgsz=large';
+  //var path = '/search/images?p=' + random1 + '+' + random2 + '&imgsz=large';
+  var path = '/search?tbm=isch&q=' + random1 + '+' + '&tbs=isz:l'
   var options = {};
   options.host = host;
   options.path = path;
@@ -86,5 +87,5 @@ function foo() {
 
 setInterval(function() {
   foo();
-  convertImg();
-}, 5000);
+  setTimeout(convertImg,5000);
+}, 10000);
